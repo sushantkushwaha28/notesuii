76e1082299baaa1cdba14124f79eaa4404c96a0c
Sushant Kushwaha <sushantkushwaha289@gmail.com>
2025-07-31 18:54:17 +0530

    make changes

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -112,14 +112,14 @@ export default function HeroSection() {
                 <div className="inset-shadow-2xs ring-background dark:inset-shadow-white/20 bg-background relative mx-auto max-w-5xl overflow-hidden rounded-2xl border p-4 shadow-lg shadow-zinc-950/15 ring-1">
                   <Image
                     className="bg-background aspect-15/8 relative hidden rounded-2xl dark:block"
-                    src="/dark.png"
+                    src="/screenshot2.png"
                     alt="app screen"
                     width="2700"
                     height="1440"
                   />
                   <Image
                     className="z-2 border-border/25 aspect-15/8 relative rounded-2xl border dark:hidden"
-                    src="/light.png"
+                    src="/screenshot1.png"
                     alt="app screen"
                     width="2700"
                     height="1440"
