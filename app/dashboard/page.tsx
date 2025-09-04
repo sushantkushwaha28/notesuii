7a6223b0dcb144c31374a51e56dd4a223daa741b
Sushant Kushwaha <sushantkushwaha289@gmail.com>
2025-09-04 23:57:07 +0530

    no changes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,13 +9,14 @@ export default async function Page() {
   return (
     <PageWrapper breadcrumbs={[{ label: "Dashboard", href: "/dashboard" }]}>
       <h1>Notebooks</h1>
-      
+
       <CreateNotebookButton />
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {notebooks.success &&
           notebooks?.notebooks?.map((notebook) => (
             <NotebookCard key={notebook.id} notebook={notebook} />
           ))}
+          
       </div>
       {notebooks.success && notebooks?.notebooks?.length === 0 && (
         <div>No notebooks found</div>
