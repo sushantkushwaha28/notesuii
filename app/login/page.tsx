434e74eda0afaff6cf3eeff7895c4800d7f8ece6
Sushant Kushwaha <sushantkushwaha289@gmail.com>
2025-09-04 23:55:29 +0530

    loginForm

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,6 @@ export default function Page() {
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <LoginForm />
-        
       </div>
     </div>
   );
