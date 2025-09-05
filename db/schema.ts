7da0938a04ce84f99f42cf3e1cadf8297bc35660
Sushant Kushwaha <sushantkushwaha289@gmail.com>
2025-09-05 21:42:28 +0530

    haha

--- db/schema.ts
+++ db/schema.ts
@@ -11,6 +11,8 @@ export const user = pgTable("user", {
     updatedAt: timestamp('updated_at').$defaultFn(() => /* @__PURE__ */ new Date()).notNull()
 });
 
+
+
 export const session = pgTable("session", {
     id: text('id').primaryKey(),
     expiresAt: timestamp('expires_at').notNull(),
