1e273d0ead983763c91b5cd785555aa6ebf93bd9
Sushant Kushwaha <sushantkushwaha289@gmail.com>
2025-10-21 23:01:53 +0530

    signupuser

--- server/users.ts
+++ server/users.ts
@@ -10,14 +10,13 @@ export const signInUser = async (email: string, password: string) => {
                 password
             },
         });
-        
+
         return { success: true, message: "Signed in successfully" };
     } catch (error) {
         const e = error as Error;
         return { success: false, message: e.message || "Failed to sign in" };
     }
 };
-
 export const signUpUser = async (email: string, password: string, name: string) => {
     try {
         await auth.api.signUpEmail({
